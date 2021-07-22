c53916f905db62467bb78a2df1ecd8d7255ebaf9
Wilhelm Lenz <wilhelm.v.lenz@gmail.com>
2021-07-22 12:50:41 +0200

    add testing code js

---
+++ testCoding/script.js
@@ -0,0 +1,33 @@
+const jonas = {
+    firstName: 'Jonas',
+    lastName: 'Schmedtmann',
+    birthYeah: 1991,
+    job: 'teacher',
+    friends: ['Michael', 'Peter', 'Steven'],
+    hasDriversLicense: true,
+
+    calcAge: function() {
+        return 2037 - this.birthYeah;
+    },
+
+    getSummary : function() {
+        return `${this.firstName} is a ${this.calcAge()}-year old ${this.job}
+        and he has ${this.hasDriversLicense === true ? 'a' : 'no'} driver's license`;
+    }
+}
+
+console.log(jonas.calcAge())
+console.log(jonas.getSummary());
+
+
+
+
+
+
+
+
+
+
+
+
+
